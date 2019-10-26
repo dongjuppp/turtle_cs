1c749329cf32a0830ede37b98f23b0fbddf448cc
ailab <jhlee17139@gmail.com>
2019-10-26 18:17:13 +0900

    191025 test

--- src/main/java/npclinic/cs/controller/AdminController.java
+++ src/main/java/npclinic/cs/controller/AdminController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class AdminController {
 
     @RequestMapping("/user_manage")
     public String admin(){
-
         System.out.println("abc");
         return "admin/user_manage";
     }
